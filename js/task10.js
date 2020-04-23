3197167c10a07e94d8a6c30a8593e900f30200e6
Islam Bolotov <islambolotov99@gmail.com>
2020-04-23 18:25:44 +0600

    secon version

--- js/task10.js
+++ js/task10.js
@@ -1,18 +1,15 @@
-/* let inpEmail = document.querySelector('#');
-let inpText = document.querySelector('.inp-text');
-let btn2 = document.querySelector('#btn-2'); */
-/* let name = document.querySelector('.name');
-let email = document.querySelector('.email');
- */
-let name = document.createElement('div')
-inp1.addEventListener('input', e => {
+
+
+
+inp1.addEventListener('change', e => {
+    let name = document.createElement('div')
     container.appendChild(name);
     name.innerText = e.target.value;
     
 })
 
-let lastName = document.createElement('h4')
-inp2.addEventListener('input', e => {
+inp2.addEventListener('change', e => {
+    let lastName = document.createElement('h4')
     container.append(lastName);
     lastName.innerText = e.target.value;
    
--- js/task11.js
+++ js/task11.js
@@ -5,12 +5,12 @@ right.addEventListener('click', () =>{
     i+=100
     kvadrad.style.marginLeft = i+'px';
     kvadrad.style.marginRight = 'auto';
-
+    kvadrad.style.transition = '1s'
 })
 
 left.addEventListener('click', () =>{
     i-=100;
     kvadrad.style.marginLeft = i+'px';
     kvadrad.style.marginRight = 'auto';
-
+    kvadrad.style.transition = '1s'
 })
